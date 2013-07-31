a80c37938b71efe8b8a78be338a56b6842a942b4
Matti Schneider <hi@mattischneider.fr>
2013-07-31 23:54:12 +0200

    Add TODOs

--- generate.sh
+++ generate.sh
@@ -21,6 +21,31 @@ function normalizeEntry {
 	then type='Method'
 	else type='Class'
 	fi
+
+	# TODO: handle static methods
+	# They are spottable as the method name repeats the class name. Ex: String.String-from
+
+	# TODO: values to treat specifically include:
+	# - Core (present as global)
+	# - Type.Generics (not a method)
+	# - Browser (object of objects)
+	# - Deprecated
+	# - Deprecated-Functions (remove? if not, prefix with `$` as when exposed)
+	# - Cookie-options
+	# - Number-Math
+	# - Element-Properties
+	# - Element-NativeEvents
+	# - Element-Events
+	# - Request.send-aliases
+	# - Request-JSON
+	# - Request-HTML
+	# - Fx-Transitions (hyphenated name + type (object))
+	# - Slick stuff are no methods, they are “selectors”
+	# - constructors (should be of type constructor, and the method name should probably not be "constructor")
+	# - Window (should not be namespaced in "window" as offered as global)
+	# - dollar / dollars -> $ / $$
+
+	# TODO: should DOMEvent be aliased to Event, as in the official doc listing?
 }
 
 

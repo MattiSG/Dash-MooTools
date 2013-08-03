11e8e75c0e45dc72fa0bd14745fa904010b08dfa
Matti Schneider <hi@mattischneider.fr>
2013-08-03 17:02:29 +0200

    Refactor tests

--- spec/generate_spec.rb
+++ spec/generate_spec.rb
@@ -1,12 +1,24 @@
 require "#{File.dirname(__FILE__)}/../normalize.rb"
 
 describe "#normalize" do
-	
+
 	subject { normalize data }
+
+	shared_examples 'parser' do
+		it 'should properly parse data' do
+			subject.should eq path: path, class_name: class_name, type: type, fragment: fragment, symbol: symbol
+		end
+	end
 
 	context 'DOMEvent constructor' do
 		let(:data) { 'mootools-core/Docs/Types/DOMEvent.md:DOMEvent Method: constructor {#DOMEvent:constructor}' }
 
-		it { should eq path: 'mootools-core/Docs/Types/DOMEvent.md', class_name: 'DOMEvent', type: 'Method', fragment: '#DOMEvent:constructor', symbol: "constructor" }
+		let(:path)			{ 'mootools-core/Docs/Types/DOMEvent.md' }
+		let(:class_name)	{ 'DOMEvent' }
+		let(:type)			{ 'Method' }
+		let(:fragment)		{ '#DOMEvent:constructor' }
+		let(:symbol)		{ 'constructor' }
+
+		it_should_behave_like 'parser'
 	end
 end

c4bef6a449d2278442d736477ad4a5d71adb6d05
Matti Schneider <hi@mattischneider.fr>
2013-08-03 16:38:27 +0200

    Extract fragment and symbol data

--- generate.rb
+++ generate.rb
@@ -8,17 +8,19 @@ SOURCE_FOLDER = 'mootools-core/Docs'
 def normalize(entry)
 	# entry.match(/([A-Za-z/-]+)\.md:([A-Za-z]\ )?([A-Za-z]+):[^{]{#[^:]+}/)
 	entry.match(/([^:]+):[^A-Za-z]*([A-Za-z ]+\ )?([^:]+)([^{]*){#(.+)}/) do
+		#         ^$1^^             ^^^^^$2^^^^^   ^$3^^  ^$4^^    $5
 		path = $1
-		class_name = $2 && $2.strip
+		class_name = $2.strip if $2
 		type = $3
-		$4
-		$5
-		$6
+		symbol = $4.strip
+		fragment = $5
 
 		{
 			path:		path,
 			class_name:	class_name,
-			type:		type
+			type:		type,
+			symbol:		symbol,
+			fragment:	fragment
 		}
 	end
 

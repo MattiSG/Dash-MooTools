298384b7b4775bb5a246dbe95a2168e6b4bc8269
Matti Schneider <hi@mattischneider.fr>
2013-08-05 23:18:12 +0200

    Convert Markdown to HTML documentation

---
+++ README.md
@@ -0,0 +1,6 @@
+Dependencies
+------------
+
+To generate the HTML files from the Markdown sources, you'll need the [Kramdown](http://kramdown.rubyforge.org/) Markdown parser:
+
+	gem install kramdown
--- application.rb
+++ application.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 
+require 'kramdown'
+require 'Fileutils'
 require_relative './src/generate_symbols'
 
 SOURCE_FOLDER = 'mootools-core/Docs'
+HTML_FOLDER = 'MooTools.docset/Contents/Resources/Documents/'
 
 
 generate_from(SOURCE_FOLDER).each do |entry|
@@ -10,4 +13,16 @@ generate_from(SOURCE_FOLDER).each do |entry|
 	entry.each do |key, value|
 		puts "#{key.to_s.rjust 15}: #{value}"
 	end
+end
+
+puts '==============='
+
+Dir.glob("#{SOURCE_FOLDER}/*/**/*.md").each do |filename|
+	puts "Converting #{filename}..."
+
+	html = Kramdown::Document.new(IO.read(filename)).to_html
+	output = File.join(HTML_FOLDER, filename.sub(SOURCE_FOLDER, '').sub('.md', '.html'))
+
+	FileUtils.mkdir_p(File.dirname(output))
+	IO.write(output, html)
 end
